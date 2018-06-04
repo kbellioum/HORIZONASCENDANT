4aa8df23e3ce55d0f5a0b57c75c20ab027b31d46
kbellioum <kbellioum@gmail.com>
2018-06-04 12:01:53 +0000

    navbar shrink

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,3 +5,11 @@
     console.log(navbar)
 
 })();
+
+$(window).scroll(function() {
+  if ($(document).scrollTop() > 50) {
+    $('nav').addClass('shrink');
+  } else {
+    $('nav').removeClass('shrink');
+  }
+});
